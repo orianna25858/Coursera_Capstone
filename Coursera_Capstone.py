491f882d565e56138f60bcf3ab565bc25f866dd1
orianna25858 <58272771+orianna25858@users.noreply.github.com>
2019-12-15 14:30:34 -0500

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1><center>This project will be mainly for Coursera Capstone Project</center></h1>
+
+# In[5]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+import numpy as np
+
+
+# In[8]:
+
+
+print ('Hello Capstone Project Course!')
+
